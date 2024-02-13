81939f82f4e31815cbbbf5d31018b5808356cb76
erkin7333 <erkinnajimov90@gmail.com>
2024-02-13 11:12:52 +0500

    Praktika4 tugadi

--- praktika4/js/script.js
+++ praktika4/js/script.js
@@ -1,5 +1,37 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const title = document.querySelector('#title')
-    const author = document.querySelector('#author')
-    const year = document.querySelector('#year')
+    const title = document.querySelector('#title'),
+         author = document.querySelector('#author'),
+         year = document.querySelector('#year'),
+         btn = document.querySelector('.btn'),
+         bookList = document.querySelector('#book-list');
+    btn.addEventListener('click', (event) => {
+        event.preventDefault()
+
+    //     Basic validation
+        if (title.value == '' && author.value == '' && year.value == '') {
+            alert("Iltimos, maʼlumotlaringizni kiriting")
+        } else {
+            const newRow = document.createElement('tr')
+
+        //     Creating new title
+            const newTitle = document.createElement('th')
+            newTitle.innerHTML = title.value
+            newRow.appendChild(newTitle)
+        //     Creating new Author
+            const newAuthor = document.createElement('th')
+            newAuthor.innerHTML = author.value
+            newRow.appendChild(newAuthor)
+        //     Creating new Year
+            const dataYear = document.createElement('th')
+            dataYear.innerHTML = year.value
+            newRow.appendChild(dataYear)
+
+        //     Displaying in UI
+            bookList.appendChild(newRow)
+            title.value = ''
+            author.value = ''
+            year.value = ''
+        }
+
+    })
 })
