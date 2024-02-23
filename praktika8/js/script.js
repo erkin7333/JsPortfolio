a141b39eee2540fabac52b8b0439d472c685ca14
erkin7333 <erkinnajimov90@gmail.com>
2024-02-23 11:17:03 +0500

    Ob-havo

--- praktika8/js/script.js
+++ praktika8/js/script.js
@@ -1,1 +1,34 @@
-alert(123456)
+// const car = {
+//     model: "Spark",
+//     color: "Black"
+// }
+//
+// console.log(car)
+// console.log(JSON.stringify(car))
+
+
+
+// =====PROMISE=====//
+
+// let isMeetingOkay = false;
+// const twoFriendsMeeting = new Promise(
+//     function (resolve, reject) {
+//         if (isMeetingOkay) {
+//             const msg = "Nima gap Developer!!!!!!!"
+//             resolve(msg)
+//         }else {
+//             const errorMsg = "Uzur kechirasiz~~~~~~~~"
+//             reject(errorMsg)
+//         }
+//     }
+// )
+//
+// twoFriendsMeeting
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error))
+//     .finally(() => console.log("Alooooo!"))
+
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json => console.log(json))
